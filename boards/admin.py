78dd77fb6be63c84e6039fb02f7babb8fc984973
BE-11-dongjae <p970626@gmail.com>
2025-07-02 18:17:45 +0900

    3일차 실습

--- boards/admin.py
+++ boards/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-
 from .models import Board
 
 
 # Register your models here.
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "writer", "date", "likes", "content", "created_at", "updated_at")
+    list_filter = ("writer", "date")
+    search_fields = ("title", "content", "writer")
+    ordering = ("-date",)
+    readonly_fields = ("writer",)
+    fieldsets = (
+        (None, {'fields': ('title', 'content')}),
+        ('추가옵션', {'fields': ('writer', 'likes', 'reviews'), 'classes': ('collapse',)}),
+    )
+    list_per_page = 10
+    actions = ('increment_likes',)
+
+    def increment_likes(self, request, queryset):
+        # 선택된 게시글들에 대해 'likes' 수를 1씩 증가
+        for board in queryset:
+            board.likes += 1
+            board.save()
+
+    increment_likes.short_description = "선택된 게시글의 좋아요 수 증가"
